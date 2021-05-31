7eb23250fba98f371220d1970ff65e7bc55ee752
jeevana2 <83947065+jeevana2@users.noreply.github.com>
2021-05-31 20:28:09 +0530

    Add files via upload

---
+++ Job (2).java
@@ -0,0 +1,17 @@
+package org.project.jobportal;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Job {
+
+	String companyName;
+	String jobTitle;
+	String language;
+	String employment;
+	String jobDescription;
+	String jobLocation;
+	String employerId;
+	String jobId;
+
+}
